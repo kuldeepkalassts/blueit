499c250001353d427933898367de7aeb47db0a05
Kuldeep Kala <kuldeep.kala@sstsinc.com>
2022-12-20 23:45:30 +0100

    Added Welcome Message

--- blueit/src/main/java/com/kk/blueit/BlueitApplication.java
+++ blueit/src/main/java/com/kk/blueit/BlueitApplication.java
@@ -8,6 +8,7 @@ public class BlueitApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlueitApplication.class, args);
+		System.out.println("Welcome to application");
 	}
 
 }

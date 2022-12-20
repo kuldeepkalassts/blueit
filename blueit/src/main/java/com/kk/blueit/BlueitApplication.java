7e77262b2cb88eae9a37f067498c8c3cef79655c
Kuldeep Kala <kuldeep.kala@sstsinc.com>
2022-12-20 22:49:49 +0100

    Initial Commit

---
+++ blueit/src/main/java/com/kk/blueit/BlueitApplication.java
@@ -0,0 +1,15 @@
+package com.kk.blueit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueitApplication.class, args);
+	}
+
+}
+
+
---
+++ blueit/src/test/java/com/kk/blueit/BlueitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kk.blueit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

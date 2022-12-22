43fc6183772c7f3fb39b0aa146cc89679e10f7fa
Kuldeep Kala <kuldeep.kala@sstsinc.com>
2022-12-22 01:27:24 +0100

    Demo for profile

--- blueit/src/main/java/com/kk/blueit/BlueitApplication.java
+++ blueit/src/main/java/com/kk/blueit/BlueitApplication.java
@@ -1,7 +1,10 @@
 package com.kk.blueit;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class BlueitApplication {
@@ -9,6 +12,14 @@ public class BlueitApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BlueitApplication.class, args);
 		System.out.println("Welcome to application");
+	}
+
+	@Bean
+	@Profile("dev")
+	CommandLineRunner runner(){
+		return args -> {
+			System.out.println("This is for dev only");
+		};
 	}
 
 }
